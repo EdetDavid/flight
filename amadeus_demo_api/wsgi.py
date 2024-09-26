e535b1239e90833c7dfd01e8db0fe885011a2ae3
Dvooskid <dvooskid1234@gmail.com>
2024-09-26 10:18:40 +0100

    Update wsgi.py

--- amadeus_demo_api/wsgi.py
+++ amadeus_demo_api/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amadeus_demo_api.settings')
 
 application = get_wsgi_application()
+app = spplication
